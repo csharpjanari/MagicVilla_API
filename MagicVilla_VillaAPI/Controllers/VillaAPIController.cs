bb92ed6444a49a65a0814a4a0abf3f1187627e94
Raul Janari <rdjanari1707@gmail.com>
2022-10-05 12:02:29 +0200

    Get Individual Villa

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -11,5 +11,12 @@ namespace MagicVilla_VillaAPI.Controllers
         {
             return VillaStore.villaList;
         }
+
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+        }
     }
 }
