f8c711b51327415b34f81e0d461e372b20dba0c7
Raul Janari <rdjanari1707@gmail.com>
2022-10-15 10:18:34 +0200

    Setup AutoMapper and MappingConfig

---
+++ MagicVilla_VillaAPI/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace MagicVilla_VillaAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Villa, VillaDTO>();
+            CreateMap<VillaDTO, Villa>();
+
+            CreateMap<Villa, VillaCreateDTO>().ReverseMap();
+            CreateMap<Villa, VillaUpdateDTO>().ReverseMap();
+        }
+    }
+}
--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -6,6 +6,8 @@ global using System.ComponentModel.DataAnnotations;
 global using Microsoft.AspNetCore.JsonPatch;
 global using System.ComponentModel.DataAnnotations.Schema;
 global using Microsoft.EntityFrameworkCore;
+global using AutoMapper;
+global using MagicVilla_VillaAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(option => {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
 });
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddControllers(option => {
     //option.ReturnHttpNotAcceptable = true;
     }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
