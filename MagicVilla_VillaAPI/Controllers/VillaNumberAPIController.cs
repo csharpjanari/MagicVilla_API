a819ba67e301fa8c237d12d96b23bde5b3dd306e
Raul Janari <rdjanari1707@gmail.com>
2022-10-21 10:32:37 +0200

    Create Controller - VillaNumberAPI

---
+++ MagicVilla_VillaAPI/Controllers/VillaNumberAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/VillaNumberAPI")]
+    public class VillaNumberAPIController : ControllerBase
+    {
+    }
+}
