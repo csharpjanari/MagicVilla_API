f8b14964a204bd045f656dc639a219d7424b1e40
Raul Janari <rdjanari1707@gmail.com>
2022-10-21 12:15:21 +0200

    VillaNumber Repository - Pattern Implementation

---
+++ MagicVilla_VillaAPI/Repository/VillaNumberRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace MagicVilla_VillaAPI.Repository
+{
+    public class VillaNumberRepository : Repository<VillaNumber>, IVillaNumberRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaNumberRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public async Task<VillaNumber> UpdateAsync(VillaNumber entity)
+        {
+            entity.UpdatedDate = DateTime.Now;
+            _db.VillaNumbers.Add(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
