5c1186f8d6fc8509cb50b1e5a48307d0cacbc63f
Raul Janari <rdjanari1707@gmail.com>
2022-11-12 10:14:09 +0100

    Add User Repository

---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
