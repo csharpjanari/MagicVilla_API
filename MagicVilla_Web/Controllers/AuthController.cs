99d5381b66e38d9508649ed7a19256cc4c251d88
Raul Janari <rdjanari1707@gmail.com>
2022-11-17 08:28:14 +0100

    Auth Controller Action Methods

--- MagicVilla_Web/Controllers/AuthController.cs
+++ MagicVilla_Web/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_Utility;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_Web.Controllers
 {
@@ -26,7 +28,19 @@ namespace MagicVilla_Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginRequestDTO obj)
         {
-            return View();
+            APIResponse response = await _authService.LoginAsync<APIResponse>(obj);
+
+            if(response != null && response.IsSuccess)
+            {
+                LoginResponseDTO model = JsonConvert.DeserializeObject<LoginResponseDTO>(Convert.ToString(response.Result));
+                HttpContext.Session.SetString(SD.SessionToken, model.Token);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", response.ErrorMessages.FirstOrDefault());
+                return View(obj);
+            }
         }
 
 
@@ -58,7 +72,9 @@ namespace MagicVilla_Web.Controllers
 
         public async Task<IActionResult> Logout()
         {
-            return View();  
+            await HttpContext.SignOutAsync();
+            HttpContext.Session.SetString(SD.SessionToken, "");
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult AccessDenied()
