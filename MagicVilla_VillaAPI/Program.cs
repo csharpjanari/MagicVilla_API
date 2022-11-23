1238c8aa0292956bb1cb34758ef4b6129db3cb25
Raul Janari <rdjanari1707@gmail.com>
2022-11-23 10:59:05 +0100

    Add Versioning to API Services

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -28,6 +28,11 @@ builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IVillaNumberRepository, VillaNumberRepository>();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1,0);
+});
 
 var key = builder.Configuration.GetValue<string>("ApiSettings:Secret");
 
