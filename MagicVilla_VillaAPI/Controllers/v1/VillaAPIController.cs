8ea6456255ff9c17c74623cabd974e219e482244
Raul Janari <rdjanari1707@gmail.com>
2022-12-05 19:43:43 +0100

    Bug Fixing

--- MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
@@ -25,7 +25,7 @@ namespace MagicVilla_VillaAPI.Controllers
         [ResponseCache(CacheProfileName = "Default30")]
         [ProducesResponseType(200)]
         public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")]int? occupancy,
-            [FromQuery]string? search, int pageSize = 2, int pageNumber = 1)
+            [FromQuery]string? search, int pageSize = 0, int pageNumber = 1)
         {
             try
             {
--- MagicVilla_VillaAPI/Repository/IRepository/IRepository.cs
+++ MagicVilla_VillaAPI/Repository/IRepository/IRepository.cs
@@ -3,7 +3,7 @@
     public interface IRepository<T> where T : class
     {
         Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, string? includeProperties = null,
-            int pageSize = 3, int pageNumber = 1);
+            int pageSize = 0, int pageNumber = 1);
         Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true, string? includeProperties = null);
         Task CreateAsync(T entity);
         Task RemoveAsync(T entity);
