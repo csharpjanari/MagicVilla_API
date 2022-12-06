2e7d27919b22b9f1a5d36de2529eca0065d85957
Raul Janari <rdjanari1707@gmail.com>
2022-12-06 18:57:21 +0100

    API Deployment

--- MagicVilla_Web/Controllers/AuthController.cs
+++ MagicVilla_Web/Controllers/AuthController.cs
@@ -1,7 +1,6 @@
 ﻿using MagicVilla_Utility;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
 namespace MagicVilla_Web.Controllers
